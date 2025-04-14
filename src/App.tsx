303620205012e3c4f8aa34fdc4dadd4d5f07c2d0
aka-jain <akashjain993@gmail.com>
2025-04-15 01:22:04 +0530

    Add project skeleton

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className='flex flex-col gap-2'>
+      <h1 className='text-center text-xl mt-1 font-medium text-gray-700'>Stage</h1>
+    </div>
+  )
+}
+
+export default App
